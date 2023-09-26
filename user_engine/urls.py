783e6ce0b31058dbf0566a870d4cebc0b1b63a94
Robert Coffie <rcoffie22@yahoo.com>
2023-09-26 10:50:32 +0000

    register

--- user_engine/urls.py
+++ user_engine/urls.py
@@ -4,5 +4,5 @@ from user_engine import views
 urlpatterns = [
     path("",views.dashboard, name="dashboard"),
     path("login_request/",views.login_request, name="login"),
-    
+    path("signup/",views.signup, name="signup")    
 ]
