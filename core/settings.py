93e291377e7973df21371b8fedebde2a55ac721b
Robert Coffie <rcoffie22@yahoo.com>
2023-10-04 12:29:17 +0000

    add secret variable

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-+j=2yixi5*bm$7f10#-)#5l226af1osj+tdmah+k$0vilyru1m"
+
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
