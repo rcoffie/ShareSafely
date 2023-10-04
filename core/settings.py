83d3cae06e2597b34854d8190de75c9de518f02c
Robert Coffie <rcoffie22@yahoo.com>
2023-10-04 14:49:32 +0000

    fixing collectstatic error

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,13 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 
 # Path where media is stored'
+
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
